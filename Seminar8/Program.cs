9da1cba6d5518914a8213c0837e8b26b1451ace3
Dmitry Vechkhayzer <dmitry.v82@mail.ru>
2022-09-15 19:33:07 +0400

    add tasks Seminar8

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -1,7 +1,5 @@
 ﻿// int a = 5;
-
 // int b = 10;
-
 // int tmp = b;
 // b = a;
 // a = tmp;
@@ -9,24 +7,9 @@
 // Console.WriteLine(a);
 // Console.WriteLine(b);
 
-// Задача55
-
-// int[,] RotateMatrix(int[,] array)
-// {
-// int[,] answer = new int[10 , 10];
-// for (int i = 0; i < 10; i++)
-// {
-//     for (int j = 0; j < 10; j++)
-//     {
-//         answer[i , j] = array [j , i];
-//     }
-// } 
-// return answer;
-// }
 
 
-
-// Задача57
+// Задача 53. Написать программу, которая поменяет первую и последнюю строки двумерного массива
 
 // Console.Write("Введите размерность m: ");
 // bool isNumber1 = int.TryParse(Console.ReadLine(), out int m);
@@ -43,9 +26,152 @@
 // Console.WriteLine();
 // int[,] result = CreateRandomArray(m, n);
 // Print2DArray(result);
+// Console.WriteLine();
+// SwapLines(result);
+// Print2DArray(result);
+
+// int[,] CreateRandomArray(int m, int n)
+// {
+//     int[,] array = new int[m, n];
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+//     return array;
+// }
+
+// void SwapLines(int[,] array)
+// {
+//     int temp = 0;
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         temp = array[0, j];
+//         array[0, j] = array[array.GetLength(1) - 1, j];
+//         array[array.GetLength(1) - 1, j] = temp;
+//     }
+//     return;
+// }
+
+// void Print2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+
+// Задача55. Написать программу, которая меняет строки на столбцы в двумерном массиве
+
+// Console.Write("Введите размерность m: ");
+// bool isNumber1 = int.TryParse(Console.ReadLine(), out int m);
+
+// Console.Write("Введите размерность n: ");
+// bool isNumber2 = int.TryParse(Console.ReadLine(), out int n);
+
+// if (!isNumber1 || n <= 0 || m <= 0 || !isNumber2)
+// {
+//     Console.WriteLine("Invalid number");
+//     return;
+// }
 
 // Console.WriteLine();
+// int[,] result = CreateRandomArray(m, n);
+// Print2DArray(result);
+// Console.WriteLine();
+// if(result.GetLength(1) == result.GetLength(0))
+// {
+//     RotateMatrix(result);
+//     PrintNew2DArray(result);
+// }
+// else
+// {
+//     Console.WriteLine("Не возможно поменять"); 
+// }
 
+// int[,] CreateRandomArray(int m, int n)
+// {
+//     int[,] array = new int[m, n];
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+//     return array;
+// }
+
+// void RotateMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         int tmp = 0;
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             tmp = array[i, j];
+//             array[i, j] = array[j, i];
+//             array[j, i] = tmp;
+//         }
+//     }
+//     return;
+// }
+
+// void Print2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void PrintNew2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[j, i]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+
+// Задача57. Составить частотный словарь элементов двумерного массива. 
+// Словарь содержит информацию сколько раз встречаются элементы в массиве.
+
+// Console.Write("Введите размерность m: ");
+// bool isNumber1 = int.TryParse(Console.ReadLine(), out int m);
+
+// Console.Write("Введите размерность n: ");
+// bool isNumber2 = int.TryParse(Console.ReadLine(), out int n);
+
+// if (!isNumber1 || n <= 0 || m <= 0 || !isNumber2)
+// {
+//     Console.WriteLine("Invalid number");
+//     return;
+// }
+
+// Console.WriteLine();
+// int[,] result = CreateRandomArray(m, n);
+// Print2DArray(result);
+// Console.WriteLine();
 // int[,] FindArray = FindRepeats(result);
 // PrintCuontArray(FindArray);
 
@@ -92,7 +218,6 @@
 //     }
 // }
 
-
 // void PrintCuontArray(int[,] array)
 // {
 //     for (int i = 0; i < array.GetLength(1); i++)
@@ -108,7 +233,6 @@
 //         }
 //         Console.WriteLine();
 //     }
-
 // }
 
 // int[,] CreateRandomArray(int m, int n)
@@ -127,7 +251,4 @@
 
 //     return array;
 // }
-
-
-// Задача59
 
